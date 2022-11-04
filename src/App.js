b29ab7f7df8fa127c52e1241cd7067db0f190492
Vllaadd <vlladdiimiir@gmail.com>
2022-11-04 01:25:47 -0400

    links added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
+
 function App() {
   return (
-    <div>Hello bookkeper 👨‍🏫 </div>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav>
+        <Link to="/invoices">Invoices</Link>
+        <br />
+        <Link to="expenses">Expenses</Link>
+      </nav>
+    </div>
   );
 }
 
