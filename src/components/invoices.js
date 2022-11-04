8d8bddc50138b55316d8d7eddfdf9636f02b7ecc
Vllaadd <vlladdiimiir@gmail.com>
2022-11-04 02:15:12 -0400

    data added and Link used to present data

--- src/components/invoices.js
+++ src/components/invoices.js
@@ -1,7 +1,15 @@
-export default function Invoices(){
-    return(
+import { Link } from "react-router-dom";
+import { getInvoices } from "./data";
+
+export default function Invoices() {
+    let invoices = getInvoices();
+    return (
         <div>
-            <h1>Invoices</h1>
+            <nav>
+                {invoices.map((invoice) => (
+                    <Link to={`/invoices/${invoice.number}`} key={invoice.number}><br />{invoice.name}</Link>
+                ))}
+            </nav>
         </div>
     )
 }
