14f85e0c607330c3bddb65d2f407645f4e21900c
Vllaadd <vlladdiimiir@gmail.com>
2022-11-06 04:02:36 -0500

    working on searchParams

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,13 +1,31 @@
 
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../components/data";
 
 export default function Invoices() {
     let invoices = getInvoices();
+    let [searchParams, setSearchParams] = useSearchParams();
     return (
         <div>
             <nav>
-                {invoices.map((invoice) => (
+                <input
+                value={searchParams.get("filter")}
+                onChange={(event)=>{
+                    let filter = event.target.value;
+                    if(filter){
+                        setSearchParams({filter});
+                    }else{
+                        setSearchParams({});
+                    }
+                }}
+                />
+                {invoices
+                .filter((invoice)=>{
+                    let filter = searchParams.get("filter");
+                    if(!filter) return true;
+                    let name = invoice.name.toLocaleLowerCase();
+                    return name.startsWith(filter.toLowerCase())
+                }).map((invoice) => (
                     <NavLink
                         to={`/invoices/${invoice.number}`}
                         key={invoice.number}
